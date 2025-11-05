9e9bc244a94a35a9fa6bd2c5e3fb23f6e538b4b6
shirisha031 <shirisha.m.031@gmail.com>
2025-11-05 20:10:55 +0530

    day 2 updates

--- django-postgres-backend/config/urls.py
+++ django-postgres-backend/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path("api/customers/", include("apps.customers.urls")),
+    path("api/v1/customers/", include("apps.customers.urls")),
     path("api/v1/sales/", include("apps.sales.urls")),
     path("api/v1/transfers/", include("apps.transfers.urls")),
     path("api/v1/compliance/", include("apps.compliance.urls")),
